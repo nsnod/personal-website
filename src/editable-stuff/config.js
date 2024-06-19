9e6482bbafbea64f85838f80db7907390bb67a53
nsnod <nsnod001@ucr.edu>
2024-06-18 18:43:19 -0700

    Footer changes

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -113,7 +113,7 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for Machine Learning and Cyber Security internships or ! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
+    "I'm currently looking for Machine Learning and Cyber Security internships or jobs! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
   email: "noahsnodgrass1@gmail.com",
 };
 
