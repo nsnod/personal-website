a4b4a0504fb2a77837100c4bafc582f47f15152c
nsnod <nsnod001@ucr.edu>
2024-06-20 14:10:49 -0700

    made the navbar hamburger white

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,55 +32,64 @@ const Navigation = React.forwardRef((props, ref) => {
   }, [navBottom, navbarDimensions, ref, scrollPosition]);
 
   return (
-    <Navbar
-      ref={navbarMenuRef}
-      className={`px-3 fixed-top ${!isTop ? "navbar-white" : "navbar-transparent"}`}
-      expand="lg"
-    >
-      <Navbar.Brand
-        className="navbar-brand"
-        href={process.env.PUBLIC_URL + "/#home"}
+    <>
+      <style>
+        {`
+          .navbar-toggler-icon {
+            background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255, 255, 255, 1)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E");
+          }
+        `}
+      </style>
+      <Navbar
+        ref={navbarMenuRef}
+        className={`px-3 fixed-top ${!isTop ? "navbar-white" : "navbar-transparent"}`}
+        expand="lg"
       >
-        {`<${mainBody.firstName} />`}
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" className="toggler" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="navbar-nav mr-auto">
-          {repos.show && (
+        <Navbar.Brand
+          className="navbar-brand"
+          href={process.env.PUBLIC_URL + "/#home"}
+        >
+          {`<${mainBody.firstName} />`}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="toggler" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="navbar-nav mr-auto">
+            {repos.show && (
+              <NavLink
+                className="nav-item lead navbar-link"
+                href={process.env.PUBLIC_URL + "/#projects"}
+              >
+                Projects
+              </NavLink>
+            )}
             <NavLink
               className="nav-item lead navbar-link"
-              href={process.env.PUBLIC_URL + "/#projects"}
+              href={about.resume}
+              target="_blank"
+              rel="noreferrer noopener"
             >
-              Projects
+              Resume
             </NavLink>
-          )}
-          <NavLink
-            className="nav-item lead navbar-link"
-            href={about.resume}
-            target="_blank"
-            rel="noreferrer noopener"
-          >
-            Resume
-          </NavLink>
-          {about.show && (
-            <NavLink
-              className="nav-item lead navbar-link"
-              href={process.env.PUBLIC_URL + "/#aboutme"}
-            >
-              About
-            </NavLink>
-          )}
-          {skills.show && (
-            <NavLink
-              className="nav-item lead navbar-link"
-              href={process.env.PUBLIC_URL + "/#skills"}
-            >
-              Skills
-            </NavLink>
-          )}
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+            {about.show && (
+              <NavLink
+                className="nav-item lead navbar-link"
+                href={process.env.PUBLIC_URL + "/#aboutme"}
+              >
+                About
+              </NavLink>
+            )}
+            {skills.show && (
+              <NavLink
+                className="nav-item lead navbar-link"
+                href={process.env.PUBLIC_URL + "/#skills"}
+              >
+                Skills
+              </NavLink>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
   );
 });
 
